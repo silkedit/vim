60aec5988051048abf7f588631b524fb37283d2b
Shinichi Katayama <shin1.katayama@gmail.com>
2016-01-07 15:25:14 +0900

    Change based on the latest SilkEdit API

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-'use strict'
+'use strict';
+
+const silkedit = require('silkedit');
 
 const MODE = {
 	CMD: Symbol(),
@@ -23,8 +25,7 @@ const keyPressHandler = (event) => {
 				}
 
 			}
-			console.log('dispatchCommand')
-			silk.dispatchCommand(event)
+			silkedit.KeymapManager.dispatch(event);
 			return true
 		case MODE.CMDLINE:
 			if (event.key == "Escape") {
@@ -67,12 +68,12 @@ function toModeText(mode) {
 }
 
 function enable() {
-	silk.installEventFilter('keypress', keyPressHandler)
-	silk.installEventFilter('runCommand', runCommandHandler)
-	silk.installEventFilter('focusChanged', focusChangedHandler)
-	silk.registerCondition("vim.mode", (operator, value) => {
+	silkedit.installEventFilter('keypress', keyPressHandler)
+	silkedit.installEventFilter('runCommand', runCommandHandler)
+	silkedit.installEventFilter('focusChanged', focusChangedHandler)
+	silkedit.registerCondition("vim.mode", (operator, value) => {
 		console.log('checking mode condition')
-		return isEnabled && silk.conditionUtils.isSatisfied(toModeText(mode), operator, value)
+		return isEnabled && silkedit.conditionUtils.isSatisfied(toModeText(mode), operator, value)
 	})
 	mode = MODE.CMD
 	onModeChanged(mode)
@@ -81,16 +82,16 @@ function enable() {
 }
 
 function disable() {
-  silk.removeEventFilter('keypress', keyPressHandler)
-  silk.removeEventFilter('runCommand', runCommandHandler)
-  silk.removeEventFilter('focusChanged', focusChangedHandler)
-  silk.unregisterCondition("vim.mode")
-  const view = silk.activeTextEditView();
+  silkedit.removeEventFilter('keypress', keyPressHandler)
+  silkedit.removeEventFilter('runCommand', runCommandHandler)
+  silkedit.removeEventFilter('focusChanged', focusChangedHandler)
+  silkedit.unregisterCondition("vim.mode")
+  const view = silkedit.API.activeTextEditView();
   if (view != null) {
     view.setThinCursor(true)
   }
 
-  silk.windows().forEach(w => w.statusBar().clearMessage())
+  silkedit.API.windows().forEach(w => w.statusBar().clearMessage())
 
   isEnabled = false
 }
@@ -108,7 +109,7 @@ function onModeChanged(newMode) {
 			return
 	}
 
-	let win = silk.activeWindow()
+	let win = silkedit.API.activeWindow()
 	if (win != null) {
 		win.statusBar().showMessage(text)
 	}
@@ -117,7 +118,7 @@ function onModeChanged(newMode) {
 }
 
 function updateCursor() {
-	const view = silk.activeTextEditView();
+	const view = silkedit.API.activeTextEditView();
 	if (view != null) {
 		const isThin = mode !== MODE.CMD
 		view.setThinCursor(isThin)
@@ -126,7 +127,7 @@ function updateCursor() {
 
 function setMode(newMode) {
 	if (mode !== newMode) {
-		const view = silk.activeTextEditView()
+		const view = silkedit.API.activeTextEditView()
 		if (newMode == MODE.CMD && view != null) {
 			view.moveCursor('left')
 		}
@@ -138,7 +139,7 @@ function setMode(newMode) {
 
 module.exports = {
 	activate: () => {
-		if (silk.config.get('vim.enable_on_startup')) {
+		if (silkedit.Config.get('vim.enable_on_startup')) {
 			enable()
 		}
 	}
